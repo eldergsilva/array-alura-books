2233e687cf19fdb1ca37cbbbd1dc0e03edc5338b
unknown <eldergsilva@gmail.com>
2024-04-10 16:57:13 -0300

    adicionado a função filtrar livros disponiveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -2,9 +2,9 @@ const botoes  = document.querySelectorAll('.btn')
 botoes.forEach( btn => btn.addEventListener('click',filtrarLivros))
 
 
-function filtrarLivros( ) {
+function filtrarLivros() {
     const elementoBtn =document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria) 
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria) 
     exibirOsLivrosNaTela(livrosFiltrados)
 }
